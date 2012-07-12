bd17db9fa26e16e7dfcc85e8d4b6837f4c82510c
ommicom <oroshii@gmail.com>
2012-07-12 10:24:30 +0700

    resolved conflict

--- mConfigurator.py
+++ mConfigurator.py
@@ -1,17 +1,10 @@
 __author__ = 'Omic'
 
 class mConfigurator():
-<<<<<<< HEAD
     __conf_name = None
     __conf_dict = dict()
-    def __init__(self,conf_name):
-        self.__conf_name = conf_name
-=======
-    __confName = None
-    __confDict = dict()
     def __init__(self,confName='config'):
         self.__confName = confName
->>>>>>> 6c63ed676c9e76ca8674747f69d822ad3ad773b9
     def LoadConfig(self):
     #load and parse config file method
         pass
