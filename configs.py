a4b1514d8315377ea6224e7d28e218973970d824
ommicom <oroshii@gmail.com>
2012-07-16 01:12:11 +0700

    conf->configs.py. Add logging

--- configs.py
+++ configs.py
@@ -2,5 +2,5 @@ server_={'smtp':'127.0.0.1','port':25,'user':'','passwd':''}
 lists={'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['mail1@mail.mail','mail2@mail.mail'],'action':'NOTICE'},
        'list2':{'mask':['*c*.txt','*d*.doc'],'recipients':['mail1@mail.mail','mail3@mail.mail'],'action':'ATTACH'}}
 bakdir = 't:\\bak'
-log = {'logmod':'FILE','format':'%(asctime)s - %(message)s'}
+log = {'logmod':'FILE'}
 
