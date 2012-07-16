f9aa7dc10884c06d43616407b39df3d230b88d3c
ommicom <oroshii@gmail.com>
2012-07-16 21:14:12 +0700

    corrected main.py (logging)

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 server_={'smtp':'172.16.2.2','port':25,'user':'','passwd':'','fromaddr':'omsk@sdm.ru'}
-lists={'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['mail1@mail.mail','mail2@mail.mail'],'action':'NOTICE'},
+lists={'list1':{'mask':['*.jpg','*b*.gif','*.txt'],'recipients':['mail1@mail.mail','mail2@mail.mail'],'action':'NOTICE'},
        'list2':{'mask':['*c*.txt','*.doc','*.xls'],'recipients':['mail1@mail.mail','mail3@mail.mail'],'action':'ATTACH'}}
 bakdir = 't:\\bak'
 logmode = 'CON'
