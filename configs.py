2b5f0b1946c8a7cfc2e381ba9e0d970873357c8d
ommicom <oroshii@gmail.com>
2012-07-20 18:27:41 +0700

    check config

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
-config = {'server':{'smtp':'172.16.2.2','port':25,'user':'','passwd':'','fromaddr':'omsk@sdm.ru','auth':'0'},
-          'lists':{'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['molodtsov@sdm.ru','omsk@sdm.ru'],'action':'notice'},
+config = {'server':{'smtp':'192.168.5.161','port':25,'user':'','passwd':'','fromaddr':'omsk@sdm.ru','auth':'0'},
+          'lists':{'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['molodtsov@sdm.ru','omsk@sdm.ru'],'action':'attach'},
                'ptk_update':{'mask':['ptk*.*'],'recipients':['molodtsov@sdm.ru'],'action':'attach'}},
           'bakdir':'t:\\b',
           'watchdir':'t:\\w',
-          'logmode':'CON'}
+          'logmode':'FILE'}
 
