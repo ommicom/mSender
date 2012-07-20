7a3082c0c2a28e420ddf5cf7763f45b18cc7d214
ommicom <oroshii@gmail.com>
2012-07-21 00:39:45 +0700

    fin configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-config = {'server':{'smtp':'smtp.gmail.com','port':587,'user':'','passwd':'','fromaddr':'omsk@sdm.ru','auth':0,'tls':0},
+config = {'server':{'smtp':'smtp.gmail.com','port':587,'user':'','passwd':'','fromaddr':'omsk@sdm.ru','auth':1,'tls':1},
           'lists':{'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['molodtsov@sdm.ru','omsk@sdm.ru'],'action':'notice'},
                'ptk_update':{'mask':['ptk*.*'],'recipients':['molodtsov@sdm.ru'],'action':'attach'}},
           'bakdir':'t:\\b',
