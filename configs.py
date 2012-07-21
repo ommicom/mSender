a1454a94343f9434caa99f938bdf4b8ae63c13a3
ommicom <oroshii@gmail.com>
2012-07-22 01:21:34 +0700

    version =0.0.4 Set logging level pre-complete

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
-config = {'server':{'smtp':'smtp.gmail.com','port':587,'user':'oroshii','passwd':'Suvaria.','fromaddr':'omsk@sdm.ru','auth':1,'tls':1},
+config = {'server':{'smtp':'smtp.gmail.com','port':587,'user':'oroshii','passwd':'','fromaddr':'oroshii@gmail.com','auth':1,'tls':1},
           'lists':{'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['omic@inbox.ru'],'action':'attach'},
                'ptk_update':{'mask':['ptk*.*'],'recipients':['molodtsov@sdm.ru'],'action':'attach'}},
           'bakdir':'t:\\b',
           'watchdir':'t:\\w',
-          'logmode':'CON'}
+          'log':{'logoutlet':'CON','loglevel':'DEBUG'}}
 
