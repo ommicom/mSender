3c8abedc231b8478d468224a92e062e449216962
ommicom <oroshii@gmail.com>
2012-07-23 18:24:35 +0700

    change config param

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
-config = {'server':{'smtp':'172.16.2.2','port':25,'user':'','passwd':'','fromaddr':'','auth':0,'tls':0},
-          'lists':{'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['omic@inbox.ru'],'action':'attach'},
+config = {'server':{'smtp':'192.168.5.161','port':25,'user':'','passwd':'','fromaddr':'omsk@sdm.ru','auth':0,'tls':0},
+          'lists':{'list1':{'mask':['*.jpg','*b*.gif'],'recipients':['omsk@sdm.ru'],'action':'attach'},
                'ptk_update':{'mask':['ptk*.*'],'recipients':['molodtsov@sdm.ru'],'action':'attach'}},
           'bakdir':'t:\\b',
           'watchdir':'t:\\w',
